39edfd2781016b7340c5ec00ad3bbd6be83b7adf
chaimete-debug <chaimete@gmail.com>
2026-04-22 15:42:39 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-const API_URL = 'COLE_AQUI_A_URL_DO_SEU_WEB_APP';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwwWGhPVk_MNjkAXcpupE3LK1WK3h4TLZ5Q8cRs57Y6haUPuI7RbCJSLRNpsHxzQJsl/exec';
